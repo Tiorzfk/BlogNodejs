120eebf81b3ff71ff99069cb82a6e8cc7a9bf273
Tioreza <tiorezafk@yahoo.com>
2016-03-25 23:27:24 +0700

    edit port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express(),
 	passport = passport();
 
 var server = http.createServer(app);
-server.listen(config.port);
+server.listen(process.env.port);
 
 module.exports = app;
 console.log(process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
