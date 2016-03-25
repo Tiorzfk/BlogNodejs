99f96a4e714cf7d5ad371a300ca87be74bb8c6b9
Tioreza <tiorezafk@yahoo.com>
2016-03-25 22:16:03 +0700

    edit style.css and server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./config/config'),
 	express = require('./config/express'),
 	passportadmin = require('./config/passportadmin'),
-	passport = require('./config/passport');
+	passport = require('./config/passport'),
+	http = require('http');
 
 var app = express(),
 	passportadmin = passportadmin(),
 	passport = passport();
 
-app.listen(config.port);
+var server = http.createServer(app);
+server.listen(config.port);
 
 module.exports = app;
 console.log(process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
