42a87114adc8f2555dfd5ab5f8c48a12e1fdc93c
Ramdhan Rizki <ramdhanrizki11@email.unikom.ac.id>
2017-01-12 23:40:32 +0700

    Update Koneksi

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,18 @@
 var port = process.env.port;
 
  var konek = {
-   host     : 'ap-cdbr-azure-southeast-b.cloudapp.net',
-   user     : 'b065bc94f582d8',
-   password : '67928ce1',
-   port 	: 3306,
-   database : 'acsm_960a6532c696724'
+   //host     : 'ap-cdbr-azure-southeast-b.cloudapp.net',
+   //user     : 'b065bc94f582d8',
+   //password : '67928ce1',
+   //port 	: 3306,
+   //database : 'acsm_960a6532c696724'
+  
+  
+    host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
+    user     : 'bce28b46e34219',
+    password : '3ddb60cd',
+    port 	: 3306,
+    database : 'comrades'
  };
 
 module.exports = {
